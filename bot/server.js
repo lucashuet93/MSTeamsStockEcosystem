a9fe15731871e92d3500d042ffeef63babd2fb09
lucashuet93 <lucashh@microsoft.com>
2017-07-24 14:45:26 -0700

    connect to luis model, recognizers for 3 intents

--- bot/server.js
+++ bot/server.js
@@ -21,3 +21,31 @@ let bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 const LUISKey = secrets.LUISKey;
+const LUISEndpoint = secrets.LUISEndpoint;
+
+let recognizer = new builder.LuisRecognizer(LUISEndpoint);
+bot.recognizer(recognizer);
+
+bot.dialog('/getPrice', [
+    (session, args, next) => {
+        session.send('You wanted to get price!');
+    }
+]).triggerAction({
+    matches: 'GetPrice'
+});
+
+bot.dialog('/buy', [
+    (session, args, next) => {
+        session.send('You wanted to buy!');
+    }
+]).triggerAction({
+    matches: 'Buy'
+});
+
+bot.dialog('/sell', [
+    (session, args, next) => {
+        session.send('You wanted to sell!');
+    }
+]).triggerAction({
+    matches: 'Sell'
+});

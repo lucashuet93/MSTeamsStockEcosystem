e75a8c97dbb11f0e12a0674a5f7b5d3d92c632ae
lucashuet93 <lucashh@microsoft.com>
2017-07-25 12:34:08 -0700

    add future api routes

--- api/api.js
+++ api/api.js
@@ -4,10 +4,49 @@ const initRestApi = (router) => {
     router.get('/', function (req, res) {
         res.json({ data: 'API is working' });
     });
-    // GET-WEEKS ROUTES ------------------------------------------------------------
-    // =============================================================================
-    router.route('/allUsers')
+	
+    router.route('/loginUser')
         .get((req, res) => {
+            dbQueries.getUsers((response) => {
+                res.json({
+                    data: response
+                });
+            })
+        });
+    router.route('/createUser')
+        .post((req, res) => {
+            dbQueries.getUsers((response) => {
+                res.json({
+                    data: response
+                });
+            })
+        });
+    router.route('/updateUserCapitalRemaining')
+        .post((req, res) => {
+            dbQueries.getUsers((response) => {
+                res.json({
+                    data: response
+                });
+            })
+        });
+    router.route('/buyNewShares')
+        .post((req, res) => {
+            dbQueries.getUsers((response) => {
+                res.json({
+                    data: response
+                });
+            })
+        });
+    router.route('/sellAllShares')
+        .post((req, res) => {
+            dbQueries.getUsers((response) => {
+                res.json({
+                    data: response
+                });
+            })
+        });
+    router.route('/updateShares')
+        .post((req, res) => {
             dbQueries.getUsers((response) => {
                 res.json({
                     data: response

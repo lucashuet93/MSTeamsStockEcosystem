e0af47bf8d915140fd89b17dc1eafdaa51711cc6
lucashuet93 <lucashh@microsoft.com>
2017-07-25 15:26:04 -0700

    route to noUser instead

--- bot/server.js
+++ bot/server.js
@@ -100,35 +100,35 @@ bot.dialog('/createAccount', [
                 session.send(`Thanks ${user.firstname}, you're all set to go now!`)
                 session.endDialog();
             })
-
     }
 ])
 
 bot.dialog('/getPrice', [
     (session, args, next) => {
-        if(!session.userData.user){
+        if (!session.userData.user) {
             session.send("Hmm I don't have you logged in yet.")
-            session.replaceDialog('/login')
-        }
-        let companyEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'Company');
-        if (companyEntityObject === null) {
-            session.beginDialog('/none')
+            session.replaceDialog('/noUser')
         } else {
-            let company = companyEntityObject.entity
-            if (company === null) {
-                session.send(`Hmm, it looks like you are trying to find a stock price but I didn't understand the company name/abbreviation. Please try again.`)
+            let companyEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'Company');
+            if (companyEntityObject === null) {
+                session.beginDialog('/none')
             } else {
-                let priceHistory = stockHelper.getStockPrice(company)
-                    .then((res) => {
-                        let priceHistory = res.data['Time Series (1min)']
-                        if (!priceHistory) {
-                            session.send(`Hmm, it looks like you are trying to find a stock price but I didn't understand the company name/abbreviation. Please try again.`)
-                        } else {
-                            let priceObject = priceHistory[Object.keys(priceHistory)[0]];
-                            let mostRecentPrice = priceObject['4. close']
-                            session.send(`${company}'s share price is currently at ${mostRecentPrice}!`);
-                        }
-                    })
+                let company = companyEntityObject.entity
+                if (company === null) {
+                    session.send(`Hmm, it looks like you are trying to find a stock price but I didn't understand the company name/abbreviation. Please try again.`)
+                } else {
+                    let priceHistory = stockHelper.getStockPrice(company)
+                        .then((res) => {
+                            let priceHistory = res.data['Time Series (1min)']
+                            if (!priceHistory) {
+                                session.send(`Hmm, it looks like you are trying to find a stock price but I didn't understand the company name/abbreviation. Please try again.`)
+                            } else {
+                                let priceObject = priceHistory[Object.keys(priceHistory)[0]];
+                                let mostRecentPrice = priceObject['4. close']
+                                session.send(`${company}'s share price is currently at ${mostRecentPrice}!`);
+                            }
+                        })
+                }
             }
         }
     }
@@ -139,25 +139,27 @@ bot.dialog('/getPrice', [
 
 bot.dialog('/buy', [
     (session, args, next) => {
-        if(!session.userData.user){
+        if (!session.userData.user) {
             session.send("Hmm I don't have you logged in yet.")
-            session.replaceDialog('/login')
-        }
-        let companyEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'Company');
-        let amountEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.number');
-        if (companyEntityObject === null) {
-            session.beginDialog('/none')
+            session.replaceDialog('/noUser')
         } else {
-            if (amountEntityObject === null) {
-                session.send(`Hmm, it looks like you are trying to buy stock but I didn't understand. Please try again.`)
-            }
-            let company = companyEntityObject.entity
-            let amount = amountEntityObject.entity
-            if (company === null || amount === null) {
-                session.send(`Hmm, it looks like you are trying to buy stock but I didn't understand. Please try again.`)
+            let companyEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'Company');
+            let amountEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.number');
+            if (companyEntityObject === null) {
+                session.beginDialog('/none')
             } else {
-                session.send(`You wanted to buy ${amount} shares of ${company}!`);
+                if (amountEntityObject === null) {
+                    session.send(`Hmm, it looks like you are trying to buy stock but I didn't understand. Please try again.`)
+                }
+                let company = companyEntityObject.entity
+                let amount = amountEntityObject.entity
+                if (company === null || amount === null) {
+                    session.send(`Hmm, it looks like you are trying to buy stock but I didn't understand. Please try again.`)
+                } else {
+                    session.send(`You wanted to buy ${amount} shares of ${company}!`);
+                }
             }
+
         }
     }
 ]).triggerAction({
@@ -166,24 +168,25 @@ bot.dialog('/buy', [
 
 bot.dialog('/sell', [
     (session, args, next) => {
-        if(!session.userData.user){
+        if (!session.userData.user) {
             session.send("Hmm I don't have you logged in yet.")
-            session.replaceDialog('/login')
-        }
-        let companyEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'Company');
-        let amountEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.number');
-        if (companyEntityObject === null) {
-            session.beginDialog('/none')
+            session.replaceDialog('/noUser')
         } else {
-            if (amountEntityObject === null) {
-                session.send(`Hmm, it looks like you are trying to sell stock but I didn't understand. Please try again.`)
-            }
-            let company = companyEntityObject.entity
-            let amount = amountEntityObject.entity
-            if (company === null || amount === null) {
-                session.send(`Hmm, it looks like you are trying to sell stock but I didn't understand. Please try again.`)
+            let companyEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'Company');
+            let amountEntityObject = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.number');
+            if (companyEntityObject === null) {
+                session.beginDialog('/none')
             } else {
-                session.send(`You wanted to sell ${amount} shares of ${company}!`);
+                if (amountEntityObject === null) {
+                    session.send(`Hmm, it looks like you are trying to sell stock but I didn't understand. Please try again.`)
+                }
+                let company = companyEntityObject.entity
+                let amount = amountEntityObject.entity
+                if (company === null || amount === null) {
+                    session.send(`Hmm, it looks like you are trying to sell stock but I didn't understand. Please try again.`)
+                } else {
+                    session.send(`You wanted to sell ${amount} shares of ${company}!`);
+                }
             }
         }
     }
@@ -193,11 +196,12 @@ bot.dialog('/sell', [
 
 bot.dialog('/none', [
     (session, args, next) => {
-        if(!session.userData.user){
+        if (!session.userData.user) {
             session.send("Hmm I don't have you logged in yet.")
-            session.replaceDialog('/login')
+            session.replaceDialog('/noUser')
+        } else {
+            session.send(`Hmm I didn't understand that. Please try again.`);
         }
-        session.send(`Hmm I didn't understand that. Please try again.`);
     }
 ]).triggerAction({
     matches: 'None'

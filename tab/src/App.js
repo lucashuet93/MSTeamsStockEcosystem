79d31623d29fad05ef47e07f0f419b8617392176
lucashuet93 <lucashh@microsoft.com>
2017-07-26 15:21:39 -0700

    create structure of header/content

--- tab/src/App.js
+++ tab/src/App.js
@@ -4,13 +4,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="overview">
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="content">
+        </div>
       </div>
     );
   }

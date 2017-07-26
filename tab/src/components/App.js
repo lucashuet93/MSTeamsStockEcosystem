94168f1c322959763ccff941a9e4d1b251c7ec91
lucashuet93 <lucashh@microsoft.com>
2017-07-26 16:03:01 -0700

    render skeleton login comp when no user

--- tab/src/components/App.js
+++ tab/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../assets/App.css';
 import Content from './Content'
 import Overview from './Overview'
+import Login from './Login'
 
 class App extends Component {
 	constructor(p) {
@@ -11,15 +12,29 @@ class App extends Component {
 			portfolio: null
 		}
 	}
-	render() {
+	renderContent() {
 		return (
 			<div>
 				<div className="overview">
-					<Overview portfolio={this.state.portfolio}/>
+					<Overview portfolio={this.state.portfolio} />
 				</div>
 				<div className="content">
 					<Content portfolio={this.state.portfolio} />
 				</div>
+			</div>
+		)
+	}
+	renderLogin() {
+		return (
+			<div>
+				<Login />
+			</div>
+		)
+	}
+	render() {
+		return (
+			<div>
+				{ this.state.user == null ? this.renderLogin() : this.renderContent() }
 			</div>
 		);
 	}

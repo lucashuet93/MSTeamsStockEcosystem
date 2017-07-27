0b24a75c586e3263d4a17ceb0ab27f4f7731adaf
lucashuet93 <lucashh@microsoft.com>
2017-07-27 09:46:17 -0700

    stock helpers for daily/monthly/current price

--- tab/src/helpers/stockHelper.js
+++ tab/src/helpers/stockHelper.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
 import { AlphaVantageAPIKey } from '../assets/secrets'
 
-export const getStockPrice = (symbol) => {
-	let dailyTimeSeriesRoute = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&outputsize=full&apikey=${AlphaVantageAPIKey}`
+export const getMinuteTimeSeries = (symbol) => {
+	let minuteTimeSeriesRoute = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&outputsize=full&apikey=${AlphaVantageAPIKey}`
+	return axios.get(minuteTimeSeriesRoute)	
+}
+
+export const getDailyTimeSeries = (symbol) => {
+	let dailyTimeSeriesRoute = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${AlphaVantageAPIKey}`
 	return axios.get(dailyTimeSeriesRoute)	
+}
+
+export const getMonthlyTimeSeries = (symbol) => {
+	let monthlyTimeSeriesRoute = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${AlphaVantageAPIKey}`
+	return axios.get(monthlyTimeSeriesRoute)	
 }

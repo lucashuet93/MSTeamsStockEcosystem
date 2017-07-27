2476885726a70b429f624307a5b6c1a82e82989a
lucashuet93 <lucashh@microsoft.com>
2017-07-27 11:50:42 -0700

    create controlled form in operations

--- tab/src/components/Operations.js
+++ tab/src/components/Operations.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
+import { getPortfolio, buyNewShares, sellAllShares, updateShares } from '../helpers/apiHelper'
+import { TextField, Button, Dropdown } from 'office-ui-fabric-react'
 
 class Operations extends Component {
 	constructor(p) {
 		super(p)
+		this.state = {
+			operationKey: "BUY",
+			shares: "",
+			orderTotal: "",
+		}
+		this.order = this.order.bind(this)
+		this.sharesChanged = this.sharesChanged.bind(this)
+		this.operationChanged = this.operationChanged.bind(this)
+	}
+	sharesChanged(text) {
+		let numShares = parseInt(text)
+		let total = numShares * this.props.currentPrice;
+		this.setState({
+			shares: numShares,
+			orderTotal: total
+		})
+	}
+    operationChanged(obj) {
+        this.setState({
+            operationKey: obj.text
+        })
+    }
+	order(){
+		console.log(this.state)
 	}
 	render() {
+        let vals = ["BUY", "SELL"]
+        let options = vals.map(v => {
+            return {
+                key: v,
+                text: v
+            }
+        })
 		return (
-			<div>
-				Operations
+			<div className="operationsContainer">
+				<Dropdown
+					label='Operation'
+					options={options}
+					onChanged={this.operationChanged.bind(this)}
+					selectedKey={this.state.operationKey}
+				/>
+				<TextField
+					onChanged={this.sharesChanged.bind(this)}
+					label="Shares"
+					className="ms-font-m"
+					value={this.state.shares} />
+				<TextField
+					label="Order Total"
+					className="ms-font-m"
+					disabled={true}
+					value={this.state.orderTotal} />
+				<div className="formFooterDiv">
+					<Button
+						text="Sign Up"
+						onClick={() => this.order()}
+					/>
+				</div>
 			</div>
 		);
 	}

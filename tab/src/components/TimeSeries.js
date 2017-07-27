2242c8c92ccae5cdf119108aebc2252614978652
lucashuet93 <lucashh@microsoft.com>
2017-07-27 10:16:31 -0700

    conditional rendering in time series if price info loaded yet

--- tab/src/components/TimeSeries.js
+++ tab/src/components/TimeSeries.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class TimeSeries extends Component {
+	constructor(p){
+		super(p)
+	}
+	renderLoading(){
+		return (
+			<div>Loading</div>
+		)
+	}
+	renderPriceInfo(){
+		return (
+			<div>Info Found</div>
+		)
+	}
 	render() {
+		let allPriceInfo = this.props.currentPrice && this.props.dailyTimeSeries && this.props.monthlyTimeSeries ? true : false;
+		if(allPriceInfo === true){
+			console.log('TS', this.props)
+		}
 		return (
 			<div>
-				TimeSeries
+				{allPriceInfo === true ? this.renderPriceInfo() : this.renderLoading()}
       		</div>
 		);
 	}

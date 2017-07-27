64e117f79693bc60ef2ca0a8f28a8e36ab61f94b
lucashuet93 <lucashh@microsoft.com>
2017-07-27 09:32:35 -0700

    add search bar to marketplace, control values

--- tab/src/components/Marketplace.js
+++ tab/src/components/Marketplace.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 import Operations from './Operations'
 import TimeSeries from './TimeSeries'
+import { SearchBox } from 'office-ui-fabric-react'
 
 class Marketplace extends Component {
-	constructor(p) {	
-		super(p)
+	constructor(p) {
+		super(p);
+		this.state = {
+			searchValue: ""
+		}
+		this.onChange = this.onChange.bind(this)
+		this.onSearch = this.onSearch.bind(this)
 	}
+    onChange(newValue) {
+        let lcString = newValue.toLowerCase();
+        this.setState({
+            searchValue: lcString
+        })
+    }
+    onSearch(newValue) {
+        let lcString = newValue.toLowerCase();
+		console.log(newValue)
+    }
 	render() {
 		return (
 			<div>
 				<div className="market marketSearch">
-					SearchBar
+					<SearchBox
+						className="ms-font-m-plus"
+						onChange={(newValue) => this.onChange(newValue)}
+						onSearch={(newValue) => this.onSearch(newValue)}
+					/>
 				</div>
 				<div className="market marketTimeSeries">
 					<TimeSeries />
 				</div>
 				<div className="market marketOperations">
-					<Operations portfolio={this.props.portfolio}/>
+					<Operations portfolio={this.props.portfolio} />
 				</div>
 			</div>
 		);

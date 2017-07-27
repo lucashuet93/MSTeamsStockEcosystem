7579b324c648b1317fa697000427961d1b3843d2
lucashuet93 <lucashh@microsoft.com>
2017-07-26 17:53:15 -0700

    install office ui fabric, style overview

--- tab/src/components/Statistic.js
+++ tab/src/components/Statistic.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-
+import { Label } from 'office-ui-fabric-react'
 class Statistic extends Component {
 	constructor(p) {
 		super(p)
 	}
 	render() {
-		let value = `${this.props.symbol} ${this.props.value}`
+		let value = `${this.props.symbol} ${this.props.value}`;
 		return (
 			<div className="statistic">
 				<div className="statHeader">
-					{this.props.header}
+					<span className="ms-font-xxl ms-fontWeight-semibold">{this.props.header}</span>
 				</div>
 				<div className="statValue">
-					{value}
+					<span className="ms-font-xl">{value}</span>
 				</div>
 			</div>
 		);

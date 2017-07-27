d2e57a5a46121419c50d8fd610a8285ad3de0d0f
lucashuet93 <lucashh@microsoft.com>
2017-07-27 16:51:30 -0700

    reducer prepared to handle stats

--- tab/src/reducers/StocksReducer.js
+++ tab/src/reducers/StocksReducer.js
@@ -1,14 +1,17 @@
 
-import { LOAD_PORTFOLIO } from '../actions'
+import { LOAD_PORTFOLIO, UPDATE_STATISTICS } from '../actions'
 
 const initialState = {
-    portfolio: null
+    portfolio: null,
+    statistics: null
 };
 
 const StocksReducer = (state = initialState, action) => {
     switch(action.type) {
         case LOAD_PORTFOLIO:
 			return { ...state, portfolio: action.portfolio }
+        case UPDATE_STATISTICS:
+			return { ...state, statistics: action.statistics }
         default:
             return state;
     }

013b30ea25e01bfbe11dcb74ad3e6b3dbf9167ec
lucashuet93 <lucashh@microsoft.com>
2017-07-26 19:06:50 -0700

    display table of current holdings

--- tab/src/components/MyPortfolio.js
+++ tab/src/components/MyPortfolio.js
@@ -1,16 +1,93 @@
 import React, { Component } from 'react';
+import { DetailsList, CommandButton, Link, CheckboxVisibility, IColumn, SearchBox } from 'office-ui-fabric-react';
+
+let columns = [
+    {
+        key: 'company',
+        name: 'Company',
+        fieldName: 'Company',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true
+    },
+    {
+        key: 'numShares',
+        name: '# Shares',
+        fieldName: 'NumShares',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true
+    },
+    {
+        key: 'averageInitialPrice',
+        name: 'Average Initial Price',
+        fieldName: 'SharePrice',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true
+    },
+    {
+        key: 'currentPrice',
+        name: 'Current Price',
+        fieldName: 'CurrentPrice',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true
+    },
+    {
+        key: 'growth',
+        name: 'Growth',
+        fieldName: 'Growth',
+        minWidth: 100,
+        maxWidth: 200,
+        isResizable: true
+    },
+];
 
 class MyPortfolio extends Component {
 	constructor(p) {
 		super(p)
+		this.state = {
+			searchValue: ""
+		}
 	}
-	render() {
-		return (
-			<div>
-				MyPortfolio
-			</div>
-		);
-	}
+	renderItemColumn(item, index, column) {
+        let self = this;
+        let fieldContent = item[column.fieldName];
+        switch (column.key) {
+            case 'turns':
+                return <span className='ms-font-m-plus'>{fieldContent.turns.length}</span>;
+            default:
+                return <span className='ms-font-m-plus'>{fieldContent}</span>;
+        }
+    }
+    renderStockItems() {
+        let lcString = this.state.searchValue.toLowerCase();
+        let filteredStocks = this.props.portfolio ? this.props.portfolio.filter((stockObj) => {
+            return true
+        }) : [];
+        return filteredStocks;
+    }
+    render() {
+        let stockItems = this.renderStockItems();
+		console.log(stockItems)
+        return (
+            <div className="myPortfolio">
+                <SearchBox
+                    className="ms-font-m-plus"
+                    onChange={(newValue) => this.onChange(newValue)}
+                    onSearch={(newValue) => this.onChange(newValue)}
+                />
+                <DetailsList
+                    className="ms-font-m-plus"
+                    items={stockItems}
+                    columns={columns}
+                    checkboxVisibility={CheckboxVisibility.hidden}
+                    onRenderItemColumn={this.renderItemColumn.bind(this)}
+                    onActiveItemChanged={() => console.log("we need to come up with some other view when looking at past sessions")}
+                />
+            </div>
+        );
+    }
 }
-
 export default MyPortfolio;

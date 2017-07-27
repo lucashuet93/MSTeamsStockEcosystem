d18069cdfa4cb3ee62068bb6590e25d095bfb3fb
lucashuet93 <lucashh@microsoft.com>
2017-07-27 16:36:11 -0700

    action creators ready to dispatch methods

--- tab/src/actions/index.js
+++ tab/src/actions/index.js
@@ -2,9 +2,16 @@
 export const LOAD_USER = 'LOAD-USER';
 export const LOAD_PORTFOLIO = 'LOAD_PORTFOLIO'
 
-export const test = () => {
+export const loadUser = (user) => {
 	return {
-		type: "TEST",
-		payload: "test"
+		type: LOAD_USER,
+		user: user
+	}
+}
+
+export const loadPortfolio = (portfolio) => {
+	return {
+		type: LOAD_PORTFOLIO,
+		portfolio: portfolio
 	}
 }
